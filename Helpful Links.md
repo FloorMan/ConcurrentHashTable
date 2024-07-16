edf2b128c64fc96e91286744c4054538339b55b9
Diego Aguirre <diego0610018657@gmail.com>
2024-07-16 01:02:13 -0400

    Updated chash.c and added Links
    Added links to threads, hashtables, etc... as well as added more helper functions for the hashtable and threads

--- Helpful Links.md
+++ Helpful Links.md
@@ -5,7 +5,16 @@
 * [Linked answer for ktinker for command line output](https://stackoverflow.com/questions/9927821/cookbook-gui-interface-for-a-command-line-script/9928345)
 
 ## Helpful Links for HashTables
+* [How to implement a hashtable in C](https://benhoyt.com/writings/hash-table-in-c/)
 
+## Links for Threads
+* [unistd.h man page](https://pubs.opengroup.org/onlinepubs/7908799/xsh/unistd.h.html)
+
+## Links for Implementing Locks
+* [Code for compare-and-swap locks from OS book](https://github.com/remzi-arpacidusseau/ostep-code/tree/master/threads-locks) 
+* [Example of Linked List with locks](https://chameerawijebandara.wordpress.com/2014/07/12/linked-list-with-read-write-locks-for-the-entire-linked-list-in-c/)
+* [CH 28 OSTEP: Locks](https://pages.cs.wisc.edu/~remzi/OSTEP/threads-locks.pdf)
+* [CH 29 OSTEP: Lock-Based COncurrent Data Structures](https://pages.cs.wisc.edu/~remzi/OSTEP/threads-locks.pdf)
 
 ## Jenkins Hash Function
 * [Jenkins Hash Wiki](https://en.wikipedia.org/wiki/Jenkins_hash_function)
--- chash.c
+++ chash.c
@@ -1,40 +1,55 @@
+/*
+                            ⚠️⚠️   IMPORTANT   ⚠️⚠️
+When using pthread.h, we need to compile the program with the flag -lpthread
+                    ╭―――――――------------------―――――――╮
+                    │ ~$ gcc  chash.c  -lpthread     │
+                    ╰―――――――------------------―――――――╯
+*/
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 // Uncomment out the include paths when compiling with linux and comment out the windows include path.
 // Reverse it for windows.
-//#include <pthread.h> // Used for creating threads
-//#include <unistd.h> // Used for the sleep() function
+#include <pthread.h> // Used for creating threads
+#include <unistd.h> // Used for the sleep() function
 
-#include <libunistd/unistd/unistd.h>
-#include <libunistd/unistd/pthread.h>
+//#include <libunistd/unistd/unistd.h>
+//#include <libunistd/unistd/pthread.h>
 
 
 // "Libunistd supports all the common Linux POSIX calls,⚠️⚠️ EXCEPT FORK(). PLEASE DON'T FORK ⚠️⚠️. Use C++ standard threads or libunistd's POSIX pthreads instead.""
 
 
+// Defines the size of our hashTable
+#define TABLE_SIZE 1000
+
+// Defines the structure for each hash record
 typedef struct hash_struct
 {
   uint32_t hash;
   char name[50];
   uint32_t salary;
   struct hash_struct *next;
-} hashRecord;
+} HashRecord;
+
+
+// Defines the structure for the entire hash table.
+typedef struct hashtable_struct
+{
+  int count;
+  int size;
+  HashRecord ** records;
+} HashTable;
+
 
 // Holds thread number, name, status, etc...
 typedef struct thread_struct
 {
   int threadNum;
-  char threadName[50]
-
+  char threadName[50];
 } thread_t; 
 
-
-
-
-// Definition for the HashTable
-// Need to add size
-hashRecord HashTable[];
 
 // Definition for the Lock
 // pthread luckily provides us with a handy Read-Write lock struct
@@ -42,21 +57,50 @@ pthread_rwlock_t lock;
 
 int main(){
 
-  // We start off by reading in all of the commands from the 
-
-
-
-
-
-
+  // We start off by reading in all of the commands from the commands.txt file
 
 
     return 0;
 }
 
 
+// =================================================================================
+//  ------------------------- HASHTABLE HELPER FUNCTIONS --------------------------
+// =================================================================================
+
+// Returns a new, empty HashTable
+HashTable * initHashTable(){
+  HashTable * newHashTable = (HashTable*)malloc(sizeof(HashTable));
+  newHashTable->count = 0;
+  newHashTable->size = TABLE_SIZE;
+
+  // Allocating space for the main array of HashRecords, holds TABLE_SIZE amt of HashRecords
+  HashRecord ** newRecordArray = (HashRecord **)malloc(sizeof(HashRecord *) * TABLE_SIZE);
+  newHashTable->records = newRecordArray;
+
+  return newHashTable;
+}
+
+// 0 = added to table | 1 = alr in table, updated
+// Adds a name to the table, acquires a write lock, updates node, releases lock.
+int insert(char * name, uint32_t salary){
 
 
+  return 0;
+}
+
+void delete(char * name){
+
+}
+
+void search(char * name){
+
+}
+
+
+// =================================================================================
+//  ------------------------- THREAD HELPER FUNCTIONS --------------------------
+// =================================================================================
 
 // This function creates a thread and returns it
 thread_t * createThread(){
