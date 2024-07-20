b794a40f8e32b4bc6d341834f3681b22657dba26
Kota Ramsey <66634314+Jeddsh@users.noreply.github.com>
2024-07-20 11:35:12 -0400

    Updated threadHandler.c with r/w locks around CS

--- threadHandler.c
+++ threadHandler.c
@@ -1,5 +1,7 @@
 #include "threadHandler.h"`
 
+pthread_rwlock_t lock = PTHREAD_RWLOCK_INITIALIZER;
+
   // Initialize commands (needed to be fixed accordingly reading input from files)
   for (int i = 0; i < numThreads; i++){
     strcpy(cmds[i].command, "insert");
@@ -22,21 +24,61 @@
 
 
 void* handleCommand(void* arg) {
+    pthread_rwlock_t *p = (pthread_rwlock_t *)arg;  
     struct command_t *cmd = (struct command_t *)arg;
 
     if (strcmp(cmd->command, "insert") == 0) {
+      //try writer lock. if fails exit 
+      if (pthread_rwlock_wrlock(p) != 0) {
+        perror("reader_thread: pthread_rwlock_wrlock error");
+        exit(__LINE__);
+      }
       // call insert function
-       
+      insert(head, cmd->name, cmd->salary);
+      //try unlock. if fails exit
+      if (pthread_rwlock_unlock(p) != 0) {
+        perror("reader thread: pthred_rwlock_unlock error");
+        exit(__LINE__);
+      }   
     } else if (strcmp(cmd->command, "delete") == 0) {
+      //try writer lock. if fails exit 
+      if (pthread_rwlock_wrlock(p) != 0) {
+        perror("reader_thread: pthread_rwlock_wrlock error");
+        exit(__LINE__);
+      }
       // call delete function
-       
+      delete(head, cmd->name);
+      //try unlock. if fails exit
+      if (pthread_rwlock_unlock(p) != 0) {
+        perror("reader thread: pthred_rwlock_unlock error");
+        exit(__LINE__);
+      }   
 
     } else if (strcmp(cmd->command, "search") == 0) {
+      //try reader lock. if fails exit 
+      if (pthread_rwlock_rdlock(p) != 0) {
+        perror("reader_thread: pthread_rwlock_rdlock error");
+        exit(__LINE__);
+      }
       // call search function
-       
+      search(head, cmd->name);
+      //try unlock. if fails exit
+      if (pthread_rwlock_unlock(p) != 0) {
+        perror("reader thread: pthred_rwlock_unlock error");
+        exit(__LINE__);
+      }   
     } else if (strcmp(cmd->command, "print") == 0) {
+       if (pthread_rwlock_rdlock(p) != 0) {
+        perror("reader_thread: pthread_rwlock_rdlock error");
+        exit(__LINE__);
+      }
       // call print function
-      
+      printTable(head);
+      //try unlock. if fails exit
+      if (pthread_rwlock_unlock(p) != 0) {
+        perror("reader thread: pthred_rwlock_unlock error");
+        exit(__LINE__);
+      }   
     } else {
         printf("Unknown command %s\n", cmd->command);
     }
